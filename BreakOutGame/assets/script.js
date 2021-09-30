dd43cf4eb27261f87d5acf3475ca3a273676dbb9
AmanDeep9925 <amandeep58257@gmail.com>
2021-09-30 20:41:16 +0530

    Added Movement functionalities

--- BreakOutGame/assets/script.js
+++ BreakOutGame/assets/script.js
@@ -7,6 +7,9 @@ const ctx = canvas.getContext("2d");
 
 let score = 0;
 
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
 // * Rules event handlers
 
 rulesBtn.addEventListener("click", () => {
@@ -35,9 +38,33 @@ const paddle = {
   y: canvas.height - 20,
   w: 80,
   h: 10,
-  speec: 8,
+  speed: 8,
   dx: 0,
 };
+
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+// * create bricks
+
+const bricks = [];
+
+for (let i = 0; i < brickRowCount; ++i) {
+  bricks[i] = [];
+
+  for (let j = 0; j < brickColumnCount; ++j) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
 
 // * Drawing ball on canvas
 let drawBalls = () => {
@@ -61,10 +88,110 @@ let drawScore = () => {
   ctx.fillText(`Score : ${score}`, canvas.width - 100, 30);
 };
 
+let drawBricks = () => {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? "#0095dd" : "transparent";
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+};
+
+let movePaddle = () => {
+  paddle.x += paddle.dx;
+  // * Wall detection
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w;
+  }
+
+  if (paddle.x < 0) {
+    paddle.x = 0;
+  }
+};
+
+let moveBall = () => {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  //* wall collision(left/right)
+  if (ball.x + ball.size > canvas.width || ball.x - ball.size < 0) {
+    ball.dx *= -1;
+  }
+
+  // * wall collison(top/down)
+  if (ball.y + ball.size > canvas.height || ball.y - ball.size < 0) {
+    ball.dy *= -1;
+  }
+
+  if (
+    ball.x - ball.size > paddle.x &&
+    ball.x + ball.size < paddle.x + paddle.w &&
+    ball.y + ball.size > paddle.y
+  ) {
+    ball.dy = -ball.speed;
+  }
+
+  // * Bricks collision
+
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      if (brick.visible) {
+        if (
+          ball.x - ball.size > brick.x &&
+          ball.x + ball.size < brick.x + brick.w &&
+          ball.y + ball.size > brick.y &&
+          ball.y - ball.size < brick.y + brick.h
+        ) {
+          ball.dy *= -1;
+          brick.visible = false;
+        }
+      }
+    });
+  });
+};
+
 let draw = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   drawBalls();
   drawPaddle();
   drawScore();
+  drawBricks();
 };
 
-draw();
+let update = () => {
+  movePaddle();
+
+  moveBall();
+  // * Draw everything
+  draw();
+
+  requestAnimationFrame(update);
+};
+
+update();
+
+let keyDown = (e) => {
+  if (e.key === "ArrowRight" || e.key === "Right") {
+    paddle.dx += paddle.speed;
+  } else if (e.key === "ArrowLeft" || e.key === "Left") {
+    paddle.dx = -paddle.speed;
+  }
+};
+
+let keyUp = (e) => {
+  if (
+    e.key === "ArrowRight" ||
+    e.key === "Right" ||
+    e.key === "ArrowLeft" ||
+    e.key === "Left"
+  ) {
+    paddle.dx = 0;
+  }
+};
+
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
